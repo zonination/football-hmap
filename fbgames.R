0b93d5167a0df7387b75b0f017db8de89b3c5ae1
Zoni Nation <zonination@gmail.com>
2017-03-14 15:10:20 -0400

    Add files via upload

--- fbgames.R
+++ fbgames.R
@@ -1,25 +1,25 @@
 # Import data, set working directory, libraries
-setwd("~/Dropbox/R/Football")
-games<-read.csv("boxscores_game_scores.cgi_game_scores.csv", comment.char="#", stringsAsFactors=FALSE)
+# setwd("~/Dropbox/R/Football")
+games<-read.csv("games.csv", stringsAsFactors=FALSE)
 library(ggplot2)
-
-# Conver to numeric values
-games$PtsW<-as.numeric(games$PtsW)
-games$PtsL<-as.numeric(games$PtsL)
-games$Count<-as.numeric(games$Count)
+library(viridis)
 
 # Output plot
-# Recommended resolution: 1600x900 px
 ggplot(games,aes(x=PtsW,y=PtsL))+
   geom_tile(aes(fill=Count),color="black")+
-  ggtitle("")+
-  ylab("Losing Score")+
-  xlab("Winning Score")+
-  scale_fill_distiller(palette="BuGn")+
+  scale_fill_viridis(option="viridis")+ # Alternate `option="plasma"`
+#  scale_fill_distiller(palette="BuGn")+ # Uncomment to change palette to ColorBrewer
   scale_y_continuous(limits=c(-1,60),breaks=0:60)+
   scale_x_continuous(limits=c(-1,80),breaks=0:80)+
+  labs(title="Game Scores in Pro Football History",
+       subtitle="Using the results of 15,741 American Football Games", # Change this if you update the data
+       y="Winning Score",
+       x="Losing Score",
+       caption="created by /u/zonination")+
+  coord_fixed()+
   theme_bw()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1,vjust=.5))+
   theme(panel.background=element_rect(fill="#000000", color="#F0F0F0"))+
   theme(panel.grid.major=element_line(color="#252525",size=.25))+
   theme(panel.grid.minor=element_blank())
+ggsave("football-hmap.png", height=9, width=12, dpi=120, type="cairo-png")
